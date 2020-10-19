25d8fdbd1f371fbee6f81d36176f000947373b96
waltYeh <waltyeh13@gmail.com>
2020-10-19 17:57:53 +0200

    search added, F problem along k

--- run_sparse_feedback.m
+++ run_sparse_feedback.m
@@ -1,4 +1,4 @@
-options.rho=100;
-options.gam=1;
+options.rho=1;
+options.gam=0.1;
 options.reweightedIter=5;
 sparse_feedback(L_opt,diffs,b_nom,options)
--- sparse_feedback.m
+++ sparse_feedback.m
@@ -1,7 +1,8 @@
-function sparse_feedback(F,diff,b_nom,options)
+function sparse_feedback(F_in,diff,b_nom,options)
+F=F_in;
 rho=options.rho;
 gam=options.gam;
-reweighted_Max_Iter = options.reweightedIter;
+% reweighted_Max_Iter = options.reweightedIter;
 n_b=size(F,2);
 n_u=size(F,1);
 horizon=size(F,3);
@@ -19,40 +20,49 @@ eps_rel = 1.e-2;
 % method
 tolAM = 1.e-2; 
 tolNT = 1.e-3;
-ADMM_Max_Iter = 50;
+reweighted_Max_Iter =3;
+ADMM_Max_Iter = 10;
 for reweightedstep = 1 : reweighted_Max_Iter
     % Solve the minimization problem using ADMM
     for ADMMstep = 1 : ADMM_Max_Iter
         U=G-lam_sparse/rho;
         F=fmin_admm(diff,rho,F,U,b_nom,tolAM);
         V=F+lam_sparse/rho;
-        G=block_schr(V,W,gam,rho,blksize,sub_mat_size);
+        sub_mat_amount=[n_u,n_b];
+        blksize=[1;1];
+        G=block_schr(V,W,gam,rho,blksize,sub_mat_amount);
         lam_sparse=lam_sparse+rho*(F-G);
         % nn is the number of states of each subsystem
         % mm is the number of inputs of each subsystem
         % N is the number of subsystems                
-        Wnew = ones(sub_mat_size);
+        Wnew = ones(sub_mat_amount);
         mm = blksize(1);
         nn = blksize(2);            
-        eps = 1e-3;
-        for ii = 1:sub_mat_size(1)
-            for jj = 1:sub_mat_size(2)
+        eps = 1e-2;
+        for ii = 1:sub_mat_amount(1)
+            for jj = 1:sub_mat_amount(2)
                 Wnew(ii,jj) = 1 / ( norm( F( mm*(ii-1)+1 : mm*ii, ...
                     nn*(jj-1)+1 : nn*jj ),'fro' ) + eps );
             end
         end 
+        W=Wnew;
     end
 end
 
 end
-function Fnew=fmin_admm(diffs,rho,F,U,b_nom,tolAM)
+function Fnew=fmin_admm(diffs,rho,F,U,b_nom,tol)
 cuu=diffs.cuu;
+cbb=diffs.cxx;
 n_b=size(F,2);
 n_u=size(F,1);
 horizon=size(F,3)+1;
-nabla_JF=zeros(size(F));
+F_diff=zeros(size(F));
+grad_phi=zeros(size(F));
 cov_belief=zeros(n_b,n_b,horizon);
+Ffullstep=zeros(size(F));
 Fnew=zeros(size(F));
+phi=zeros(horizon-1,1);
+AM_Max_Iter = 100;
 for k=1:horizon-1
     components_amount=2;
     [x, P, w] = b2xPw(b_nom(1:42,k), 4, components_amount);
@@ -66,12 +76,67 @@ for k=1:horizon-1
         [x_ass, P_ass] = b2xP(b_nom(ind_b,k), 2);
         cov_belief(ind_b_start:ind_b_start+1,ind_b_start:ind_b_start+1,k)=P_ass;
     end
-%     nabla_JF(:,:,k)=cuu(:,:,k)*F(:,:,k)*cov_belief(:,:,k);
-    Fnew(:,:,k)=lyap(rho*inv(cuu(:,:,k)),cov_belief(:,:,k),-rho*U(:,:,k));
+    phi(k) = trace( cov_belief(:,:,k) * (cbb(:,:,k) + F(:,:,k)' * cuu(:,:,k) * F(:,:,k)) ) + ...
+                    (rho/2) * norm( F(:,:,k) - U(:,:,k), 'fro' )^2;
+    
 end
+for iter=1:1%AM_Max_Iter
+    for k=1:horizon-1
+        Ffullstep(:,:,k)=lyap(rho*inv(cuu(:,:,k)),cov_belief(:,:,k),-rho*cuu(:,:,k)\U(:,:,k));
+        F_diff(:,:,k)=Ffullstep(:,:,k)-F(:,:,k);
+        grad_phi(:,:,k)=cuu(:,:,k)*F(:,:,k)*cov_belief(:,:,k) + rho * (F(:,:,k) - U(:,:,k));
+        if trace( F_diff(:,:,k)' * grad_phi(:,:,k) ) > 1.e-10
+            error('Ftilde is not a descent direction!')
+        end 
+        if norm( grad_phi(:,:,k), 'fro' ) < tol%found necessary condition for opt, above (NC-F)
+%             break;
+        end
+        stepsize = 1;%step size will be a half for each while iteration
+        while 1
+            Ftemp = F(:,:,k) + stepsize * F_diff(:,:,k);
+            phitemp_k = trace( cov_belief(:,:,k) * (cbb(:,:,k) + Ftemp' * cuu(:,:,k) * Ftemp) ) + ...
+                            (rho/2) * norm( Ftemp - U(:,:,k), 'fro' )^2;
+            alpha = 0.3; 
+            beta  = 0.5;
+            if ~isnan(phitemp_k) && phi(k) - phitemp_k > ...
+                        stepsize * alpha * trace( - F_diff(:,:,k)' * grad_phi(:,:,k) )
+%                 stepsize
+%                 k
+                break;
+            end                
+            stepsize = stepsize * beta;
 
-F_diff=Fnew-F;
+            if stepsize < 1.e-16            
+                error('Extremely small stepsize in F-minimization step!');            
+            end 
+        end%while
+        Fnew(:,:,k) = Ftemp;    
+    % 	L = Ltemp;
+        phi(k) = phitemp_k;
+    end%k
+end%iter
 end
-function G=block_schr(V,W,gam,rho,blksize,sub_mat_size)
-    G=V;
+function G=block_schr(V,W,gam,rho,blksize,sub_mat_amount)
+G=zeros(size(V));
+horizon=size(G,3)+1;
+mm=blksize(1);
+nn=blksize(2);
+p = sub_mat_amount(1);
+q = sub_mat_amount(2);
+for k=1:horizon-1
+    for i=1:p
+        for j=1:q
+            wij = W(i,j);
+            Vij = V( mm * (i-1) + 1:mm*i, nn * (j-1) + 1:nn*j);
+            a = (gam / rho) * wij;
+            nVij = norm( Vij, 'fro' );
+            if nVij <= a
+                G( mm * (i-1) + 1:mm*i, nn * (j-1) + 1:nn*j,k) = 0;
+            else                 
+                G( mm * (i-1) + 1:mm*i, nn * (j-1) + 1:nn*j,k) = ...
+                    (1 - a/nVij) * Vij;%(14)
+            end
+        end
+    end
+end
 end
